2fcbf038558216cddf1c132a849c2dbca2946781
Akirova Sezim <112478706+sezim-akirova@users.noreply.github.com>
2023-10-08 22:16:23 +0600

    задание 6-магазин шестерочка

---
+++ task-6/lib/task6.dart
@@ -0,0 +1,186 @@
+class RawProductItem {
+  final String name;
+  final String categoryName;
+  final String subcategoryName;
+  final DateTime expirationDate;
+  final int qty;
+
+  RawProductItem(
+      {required this.name,
+      required this.categoryName,
+      required this.subcategoryName,
+      required this.expirationDate,
+      required this.qty});
+
+  @override
+  String toString() {
+    return 'RawProductItem{name: $name, categoryName: $categoryName, '
+        'subcategoryName: $subcategoryName, expirationDate: $expirationDate, qty: $qty}';
+  }
+}
+
+final products = [
+  RawProductItem(
+    name: 'Персик',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Молоко',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Кефир',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Творог',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Творожок',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Творог',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Не напитки',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Гауда',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Сыры',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 3,
+  ),
+  RawProductItem(
+    name: 'Маасдам',
+    categoryName: 'Молочные продукты',
+    subcategoryName: 'Сыры',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 2,
+  ),
+  RawProductItem(
+    name: 'Яблоко',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2023, 12, 4),
+    qty: 4,
+  ),
+  RawProductItem(
+    name: 'Морковь',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Овощи',
+    expirationDate: DateTime(2023, 12, 23),
+    qty: 51,
+  ),
+  RawProductItem(
+    name: 'Черника',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Ягоды',
+    expirationDate: DateTime(2023, 12, 25),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Курица',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2023, 12, 18),
+    qty: 2,
+  ),
+  RawProductItem(
+    name: 'Говядина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2023, 12, 17),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Телятина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2023, 12, 17),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Индюшатина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2023, 12, 17),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Утка',
+    categoryName: 'Мясо',
+    subcategoryName: 'Птица',
+    expirationDate: DateTime(2023, 12, 18),
+    qty: 0,
+  ),
+  RawProductItem(
+    name: 'Гречка',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Крупы',
+    expirationDate: DateTime(2023, 12, 22),
+    qty: 8,
+  ),
+  RawProductItem(
+    name: 'Свинина',
+    categoryName: 'Мясо',
+    subcategoryName: 'Не птица',
+    expirationDate: DateTime(2023, 12, 23),
+    qty: 5,
+  ),
+  RawProductItem(
+    name: 'Груша',
+    categoryName: 'Растительная пища',
+    subcategoryName: 'Фрукты',
+    expirationDate: DateTime(2023, 12, 25),
+    qty: 5,
+  ),
+];
+
+void main() {
+  final DateTime today = DateTime.now();
+
+  // Фильтруем товары с учетом условий
+  final List<RawProductItem> filtered = products
+      .where(
+          (element) => element.qty > 0 && element.expirationDate.isAfter(today))
+      .toList();
+
+  // Создаем структуру для хранения данных
+  final Map<String, Map<String, List<String>>> categorizedProducts = {};
+
+  // Заполняем структуру данными
+  for (final product in filtered) {
+    final category = product.categoryName;
+    final subcategory = product.subcategoryName;
+    final name = product.name;
+
+    // Создаем категорию, если ее нет
+    categorizedProducts.putIfAbsent(category, () => {});
+
+    // Создаем подкатегорию, если ее нет
+    categorizedProducts[category]!.putIfAbsent(subcategory, () => []);
+
+    // Добавляем товар в подкатегорию
+    categorizedProducts[category]![subcategory]!.add(name);
+  }
+  print(categorizedProducts);
+}
